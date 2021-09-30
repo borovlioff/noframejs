f1c20eb412ee8adcf9254fdea34436b792481192
borovlioff <borovliov.alex@ya.ru>
2021-09-30 12:52:23 +0700

    fix export Component

--- src/Component.d.ts
+++ src/Component.d.ts
@@ -1,4 +1,4 @@
-declare class Component {
+export declare class Component {
     node: HTMLElement | any;
     constructor():void
   
@@ -12,4 +12,3 @@ declare class Component {
   
     render(...args: any[]):HTMLElement;
   }
-  
